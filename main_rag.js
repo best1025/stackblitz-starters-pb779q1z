47c797d337e85263c3cfc0fec5cdc793ab21f27c
best1025 <65403918+best1025@users.noreply.github.com>
2026-06-08 05:33:29 +0000

    03_建立迷你知識庫

--- main_rag.js
+++ main_rag.js
@@ -1,11 +1,11 @@
 import { input } from "@inquirer/prompts";
-import { searchCoffee } from "./lib/qdrant.js";
+import { searchNetflix } from "./lib/qdrant.js";
 import { spinner } from "./utils/spinner.js";
 
 try {
   while (true) {
     const query = (
-      await input({ message: "請輸入你喜好的咖啡風味：" })
+      await input({ message: "請輸入要搜尋的影片內容：" })
     ).trim();
 
     if (query === "") continue;
@@ -15,16 +15,14 @@ try {
     }
 
     const spin = spinner("搜尋中...").start();
-    const results = await searchCoffee(query, 5);
+    const results = await searchNetflix(query, 5);
     spin.stop();
 
-
     for (const [i, r] of results.entries()) {
-      console.log(`\n${i + 1}. ${r.item} `);
+      console.log(`\n${i + 1}. ${r.title} (${r.type}, ${r.release_year})`);
       console.log(`   分數：${r.score.toFixed(3)}`);
+      console.log(`   分類：${r.listed_in}`);
       console.log(`   描述：${r.description}`);
-      console.log(`   價格：${r.price}`);
-
     }
     console.log();
   }
